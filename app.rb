f07d7b71281c45bbdd9116160063b70b4fd79d28
John Hayes <johnnyhayes7@gmail.com>
2019-04-23 13:43:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! THIS IS WHAT SPACE SMELLS LIKE"
+    "Welcome to your app!!!! THIS IS WHAT SPACE SMELLS LIKE/YOU WILL ALWAYS REMEMBER WHERE YOU WERE!"
   end
 
 end
